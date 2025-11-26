a9c5591983773e83fde5b937a9a3b89bc57699e1
Hoang Son <sonjiny2004@gmail.com>
2025-11-26 20:54:18 +0700

    Init project

---
+++ src/main/java/com/fashionvista/backend/FashionVistaBackendApplication.java
@@ -0,0 +1,13 @@
+package com.fashionvista.backend;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class FashionVistaBackendApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(FashionVistaBackendApplication.class, args);
+	}
+
+}
---
+++ src/test/java/com/fashionvista/backend/FashionVistaBackendApplicationTests.java
@@ -0,0 +1,13 @@
+package com.fashionvista.backend;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class FashionVistaBackendApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
